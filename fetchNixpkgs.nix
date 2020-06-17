c98c8457b85df64b4724dcdd8301b46d03f8992f
Vadim Manaenko <vadim.razorq@gmail.com>
2020-06-17 14:47:56 +0300

    Update install files

--- fetchNixpkgs.nix
+++
@@ -1,8 +0,0 @@
-{ rev    ? "6ee06dbd030824d75ce933c2813a732ed2bb47ea"             # The Git revision of nixpkgs to fetch
-, sha256 ? "12jql4dwa45kjnwfpp457sjmdrqv5i0bsrpd1lq2vhp45zwmqrdz" # The SHA256 of the downloaded data
-}:
-
-builtins.fetchTarball {
-  url = "https://github.com/airalab/airapkgs/archive/${rev}.tar.gz";
-  inherit sha256;
-}
--- release.nix
+++ release.nix
@@ -1,10 +1,9 @@
-{ nixpkgs ? import ./fetchNixpkgs.nix { }
+{ nixpkgs ? import (builtins.fetchTarball https://github.com/airalab/airapkgs/archive/nixos-unstable.tar.gz)
 , system ? builtins.currentSystem
 }:
 
 let
-  pkgs = import nixpkgs { inherit system; };
-
+  pkgs = nixpkgs { inherit system; };
 in rec {
-  package = pkgs.callPackage ./default.nix { };
+  package = pkgs.callPackage ./default.nix {  };
 }
