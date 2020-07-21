e81c0b91b58fd91467ac312047285e0644d6be36
Vadim Manaenko <vadim.razorq@gmail.com>
2020-07-21 15:07:46 +0300

    Make flash firmware as a standalone script

--- flash_firmware.py
+++ flash_firmware.py
@@ -6,6 +6,7 @@ import logging
 import tempfile
 import nacl.signing
 import os
+import sys
 import yaml
 
 
@@ -45,44 +46,44 @@ def main():
         settings = yaml.load(f.read(), Loader=yaml.FullLoader)
 
     ino = os.path.abspath(args.s)
-    with open(ino + "/src/mobile_gps.ino", "r") as f:
+    source_file = os.listdir(os.path.join(ino, "src"))[0]
+    with open(os.path.join(ino, "src", source_file), "r") as f:
         firmware = f.read()
 
-    firmware = firmware.replace("$$STASSID$$", "\"{}\"".format(settings["stassid"])) \
-                .replace("$$STAPSK$$", "\"{}\"".format(settings["stapsk"])) \
-                .replace("$$HOST$$", "\"{}\"".format(settings["host"])) \
-                .replace("$$PORT$$", str(settings["port"])) \
-                .replace("$$RXPIN$$", str(settings["rxpinsensor"])) \
-                .replace("$$TXPIN$$", str(settings["txpinsensor"])) \
-                .replace("$$RXPINGPS$$", str(settings["rxpingps"])) \
-                .replace("$$TXPINGPS$$", str(settings["txpingps"])) \
-                .replace("$$WORKPERIOD$$", str(settings["work_period"]))
+    for k, v in settings.items():
+        firmware = firmware.replace(k, str(v))
 
     tempenv = tempfile.TemporaryDirectory()
     logging.debug(f"Temporal directory is created: {tempenv}")
 
     os.chdir(tempenv.name)
     os.mkdir("src")
-    with open("src/firmware.ino", "w") as f:
+    with open(os.path.join("src", source_file), "w") as f:
         f.write(firmware)
         logging.debug(firmware)
-        logging.debug("File src/firmware.ino is written")
+        logging.debug("File {} is written".format(os.path.join("src", source_file)))
 
     os.mkdir("include")
 
     sk, vk = generate_keys()
-    with open("include/secrets.h", "w") as f:
+    with open(os.path.join("include", "secrets.h"), "w") as f:
         f.write("uint8_t signing_key[32] = {\n  ")
         f.write(write_array(sk))
         f.write(f"}};\n\nuint8_t verifying_key[32] = {{\n  ")
         f.write(write_array(vk))
         f.write("};")
 
+    shutil.copyfile(os.path.join(ino, "platformio.ini"), "platformio.ini")
 
-    shutil.copyfile(ino + "/platformio.ini", "platformio.ini")
-    print(os.getcwd())
-    os.system("python3 -m platformio run")
-    os.system("python3 -m platformio run -t upload")
+    if sys.platform.startswith("win32"):
+        os.environ["PLATFORMIO_UPLOAD_PORT"] = "COM1"
+        os.system("python -m platformio run")
+        os.system("python -m platformio run -t upload")
+    else:
+        os.system("python3 -m platformio run")
+        os.system("python3 -m platformio run -t upload")
+
+    os.chdir(ino)
 
 
 if __name__ == "__main__":
