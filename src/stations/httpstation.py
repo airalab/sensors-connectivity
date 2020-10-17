ae899e50be774d7e69c0c68ed39e8bad741347af
Vadim Manaenko <vadim.razorq@gmail.com>
2020-10-17 17:31:58 +0300

    update http station

--- src/stations/httpstation.py
+++ src/stations/httpstation.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
-import asyncio
-from asyncio import StreamReader, StreamWriter
 import threading
 import time
 import rospy
-import time
 from drivers.sds011 import SDS011_MODEL
 import json
 import cgi
+import nacl.signing
 
 from stations import IStation, StationData, Measurement, STATION_VERSION
-from collections import deque
+#from collections import deque
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-q = deque(maxlen=1)
+#q = deque()
+
+thlock = threading.RLock()
+sessions = dict()
 
 class RequestHandler(BaseHTTPRequestHandler):
 
     def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
 
     def do_HEAD(self):
@@ -28,18 +29,38 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
 
-    def parser(self, data):
+    def _generate_pubkey(self) -> str:
+        signing_key = nacl.signing.SigningKey.generate()
+        verify_key = signing_key.verify_key
+        verify_key_hex = bytes(verify_key).hex()
+        return str(verify_key_hex)
+
+    def parser(self, data) -> Measurement:
+        global sessions
+        global thlock
+        #rospy.loginfo(f"parser data: {data}")
         try:
-            for dict in data['sensordatavalues']:
-                if dict['value_type'] == 'SDS_P1':
-                    pm10 = dict['value']
-                if dict['value_type'] == 'SDS_P2':
-                    pm25 = dict['value']
-                if dict['value_type'] == 'GPS_lat':
-                    geo_lat = dict['value']
-                if dict['value_type'] == 'GPS_lon':
-                    geo_lon = dict['value']
-            public = '655a40d4b951b4fbc0c9a7658e66377f1a5ff92111f10145256e0026ab07a669'
+            for d in data["sensordatavalues"]:
+                if d["value_type"] == "SDS_P1":
+                    pm10 = d["value"]
+                if d["value_type"] == "SDS_P2":
+                    pm25 = d["value"]
+                if d["value_type"] == "GPS_lat":
+                    geo_lat = d["value"]
+                if d["value_type"] == "GPS_lon":
+                    geo_lon = d["value"]
+
+            #rospy.loginfo("After 'for'")
+
+            with thlock:
+                if self.client_address not in sessions:
+                    #rospy.loginfo(f"There is no such address: {self.client_address}")
+                    public = self._generate_pubkey()
+                else:
+                    #rospy.loginfo(f"Found such address: {self.client_address}")
+                    public = sessions[self.client_address].public
+
+            #rospy.loginfo(f"Pubkey: {public}")
             timestamp = int(time.time())
             measurement = Measurement(public,
                                     SDS011_MODEL,
@@ -55,19 +76,17 @@ class RequestHandler(BaseHTTPRequestHandler):
         return measurement
 
     def do_POST(self):
-        global q
+        global sessions
         rospy.loginfo("data is coming!")
-        ctype, pdict = cgi.parse_header(self.headers['content-type'])
-        length = int(self.headers.get('content-length'))
+        ctype, pdict = cgi.parse_header(self.headers["content-type"])
+        length = int(self.headers.get("content-length"))
         self.data = json.loads(self.rfile.read(length))
-        rospy.logdebug(self.data)
+        rospy.loginfo(self.data)
         meas = self.parser(self.data)
-        if meas:
-            q.append(meas)
-        rospy.loginfo(q[0])
+        rospy.loginfo(meas)
+        with thlock:
+            sessions[self.client_address] = meas
         self._set_headers()
-
-
 
 
 class HTTP_server(threading.Thread):
@@ -75,12 +94,11 @@ class HTTP_server(threading.Thread):
         threading.Thread.__init__(self)
         self.port = port
 
-
     def run(self):
-        rospy.loginfo('run func')
-        self.server_address = ('', self.port)
+        rospy.loginfo("run func")
+        self.server_address = ("", self.port)
         self.httpd = HTTPServer(self.server_address, RequestHandler)
-        rospy.loginfo('Starting httpd')
+        rospy.loginfo("Starting httpd")
         self.httpd.serve_forever()
 
 
@@ -90,18 +108,34 @@ class HTTPStation(IStation):
         super().__init__(config)
         port = int(config["httpstation"]["port"])
         HTTP_server(port).start()
-        self.version = f"airalab-com-{STATION_VERSION}"
+        self.version = f"airalab-http-{STATION_VERSION}"
+        self.DEAD_SENSOR_TIME = 60*60 # 1 hour
 
     def get_data(self):
-        global q
-        rospy.loginfo(q)
-        if q:
-            value = q[0]
-        else:
-            value = Measurement()
-        return [StationData(
-            self.version,
-            self.mac_address,
-            time.time() - self.start_time,
-            value
-        )]
+        global sessions
+        rospy.loginfo(sessions)
+
+        result = []
+        for k, v in self._drop_dead_sensors().items():
+            result.append(StationData(
+                self.version,
+                self.mac_address,
+                time.time() - self.start_time,
+                v
+            ))
+
+        return result
+
+    def _drop_dead_sensors(self) -> dict:
+        global sessions
+        global thlock
+        #rospy.loginfo(f"_drop_dead_sensors: {sessions}")
+        stripped = dict()
+        current_time = int(time.time())
+        with thlock:
+            for k, v in sessions.items():
+                if (current_time - v.timestamp) < self.DEAD_SENSOR_TIME:
+                    stripped[k] = v
+
+        return stripped
+
