7fc03a15a6a9c389f9d66d092133e7d3b23bf51a
tubleronchik <m.bystramovich@gmail.com>
2022-01-31 14:52:29 +0300

    fix nipkgs source wo WS

--- release.nix
+++ release.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import (builtins.fetchTarball https://github.com/tubleronchik/airapkgs/archive/fefa3fb823ca75fd4b269a40ac73dd587c62f9cb.tar.gz)
+{ nixpkgs ? import (builtins.fetchTarball https://github.com/airalab/airapkgs/archive/fefa3fb823ca75fd4b269a40ac73dd587c62f9cb.tar.gz)
 , system ? builtins.currentSystem
 }:
 
