f62d0a7ceb3e117729f3ac05d2e45fa201aaa034
tubleronchik <m.bystramovich@gmail.com>
2024-10-03 15:31:08 +0300

    feat: add timestamp to ipfs db

--- connectivity/utils/ipfs_db.py
+++ connectivity/utils/ipfs_db.py
@@ -1,3 +1,5 @@
+import time
+
 from .database import DataBase
 
 
@@ -6,8 +8,9 @@ class IPFSDB:
         self.db_class = DataBase(path_to_db_file, "ipfs")
     
     def create_table(self) -> None:
-        self.db_class.create_table("id integer PRIMARY KEY, status text, hash text")
+        self.db_class.create_table("id integer PRIMARY KEY, status text, hash text, timestamp real")
 
     def add_hash(self, hash: str) -> None:
         status = "pinned"
-        self.db_class.insert_data("status, hash", (status, hash))
+        timestamp = time.time()
+        self.db_class.insert_data("status, hash, timestamp", (status, hash, timestamp))
